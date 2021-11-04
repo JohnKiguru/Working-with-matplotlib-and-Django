4eb21ac6ecdd40b9b9498aad810169d6b69d4a8e
John Kiguru <78014995+JohnKiguru@users.noreply.github.com>
2021-11-04 23:47:45 +0300

    Update admin.py

--- sales/admin.py
+++ sales/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Product)
-
 admin.site.register(Sale)
 admin.site.register(SalesPerson)
